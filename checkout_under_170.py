0ca4a8cadcda89497487ecf281445e0a19745766
zuntue <zuntue@gmail.com>
2023-03-01 12:12:24 -0500

    its working, the summary table is made

--- checkout_under_170.py
+++ checkout_under_170.py
@@ -1,31 +1,41 @@
 # We're trying to create a summary table of each player's average check out shot count from a score of 170 oar below.
 # The table will have each player as a row and one column with the average check out shot count from  170 or below.
+import pandas as pd
+import statistics as stat
 
-# Sample data of players and their scores
-players = {
-    "Player 1": [170, 100, 80, 60, 40, 20],
-    "Player 2": [170, 150, 120, 50, 30],
-    "Player 3": [170, 140, 90, 80, 40, 30],
-    "Player 4": [170, 130, 70, 60, 20],
-    "Player 5": [170, 160, 130, 120, 70, 20],
-}
+
+df = pd.read_csv('data.csv')
+players = {key: [] for key in df['Player'].unique()}
+df.loc[:, 'Seen'] = False
+
+# Loop through rows and print each row
+for index, row in df.iterrows():
+    if int(row['Total Score']) <= 170 and row['Seen'] is False:
+        player = row['Player']
+        leg = row['Legs']
+        shots = 0
+        for loop_index, loop_row in df.iloc[index:].iterrows():
+            if loop_row['Legs'] is leg and loop_row['Player'] is player:
+                row['Seen'] = True
+                shots += 1
+                if loop_row['Total Score'] == 0:  # the player checked out
+                    players[player].append(shots)
+            if loop_row['Legs'] is not leg:
+                break
+
+
+print(players)
 
 # Function to calculate the average checkout shot count for each player
 def calculate_average_shots_to_checkout(player):
-    checkout_count = 0
-    total_score = None
-    if total_score <= 170:
-        checkout_count += 1
+    return stat.mean(players[player])
 
-        # TODO: add looping to find remaining shots by the player before either they checkout or they lose.
-
-    return checkout_count if checkout_count > 0 else 0
 
 # Create the summary table
 summary_table = {}
-for player, scores in players.items():
-    average_checkouts = calculate_average_shots_to_checkout(scores)
-    summary_table[player] = average_checkouts
+for p in players.keys():
+    average_checkouts = calculate_average_shots_to_checkout(p)
+    summary_table[p] = average_checkouts
 
 # Print the summary table
 print("Player\tAverage Check Out Shot Count")
